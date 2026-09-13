a5511099af04479e37c6ac3648e7b0cf968935dc
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2026-09-13 11:51:15 +0900

    Release Swift package 2.1.0 (#92)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "MDI", targets: ["MDI"]),
     ],
     targets: [
-        .binaryTarget(name: "MDICore", url: "https://github.com/illusions-lab/MDI/releases/download/2.0.3/MDICore.xcframework.zip", checksum: "ab6d91840673a7f54f4d76fb814f9102359780d274751e1e11ea5c411f73621e"),
+        .binaryTarget(name: "MDICore", url: "https://github.com/illusions-lab/MDI/releases/download/2.1.0/MDICore.xcframework.zip", checksum: "58b08b71ba8aaaf3c33a060e800ee9d7018707cec71c53c245579df9b34c13e0"),
         .target(name: "MDI", dependencies: ["MDICore"], path: "swift/Sources/MDI"),
         .testTarget(name: "MDITests", dependencies: ["MDI", "MDICore"], path: "swift/Tests/MDITests"),
     ]
